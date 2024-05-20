b3cabb6162310ae75a102b107ce44cb9dc6c4be3
KenKomKom <kenichikomala@gmail.com>
2024-05-20 08:57:16 +0700

    set up sonarcloud 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,16 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.sonarqube") version "4.4.1.3373"
     jacoco
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "kenkomkom_kenkomkom")
+        property("sonar.organization", "KenKomKOM")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
 
 group = "id.ac.ui.cs.advprog"
